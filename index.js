96a72619707d6108c50573557f287e77ca6d8e31
Keith Franklin <keith30895@gmail.com>
2020-08-28 10:08:57 +0530

    init

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let eh = new EncryptionHelper();
 let envArr = ['test', 'test2'];
 envArr.forEach(element => {
     if (process.env[element])
-        process.env[element] = eh.decrypt(process.env[element]); 
+        process.env[element] = eh.decrypt(process.env[element]);
 });
-Object.keys(process.env).forEach(el=>console.log(el," = ",process.env[el]));
+Object.keys(process.env).forEach(el => console.log(el, " = ", process.env[el]));
